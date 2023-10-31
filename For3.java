d8d60a5b3de788a68fe90f8e41879692d2708442
m-silvera0 <migueltenato@gmail.com>
2023-10-31 15:03:44 +0000

    sakana

---
+++ For3.java
@@ -0,0 +1,16 @@
+public class For3 extends Thread{
+    public void run(){
+        int x=0;
+        for (int i = 0; i <50; i++){
+            x=x+2;
+            System.out.println("Executando For 1");
+            try{
+                Thread.sleep(100);
+            }
+            catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("Valor Final de x: "+x);
+        }
+    }
+}
